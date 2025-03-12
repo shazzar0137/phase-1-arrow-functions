79d23a3ef8f5137dd794d4c21a62e15d8f57a81b
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-12 15:49:02 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (a, b) => a / b;
+divide (2000, 100)
+console.log(divide(2000, 100))
+
+const square = x => x**2
+square(2)
+console.log(square(4))
+
+const add = (a, b) => a + b
+add (3,4)
+console.log(add (2,3))
